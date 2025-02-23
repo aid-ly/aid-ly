2c51cce1468df6322a6a8e648fb626f22e67f1ac
Sebastiano Racca <sebastiano@racca.me>
2025-02-23 18:23:08 +0100

    feat: add privacy policy

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,5 +1,3 @@
-import { marked } from 'marked';
-
 export const sanitizeObject = <T extends object, K extends keyof T>(
 	obj: T,
 	fields: K[],
@@ -11,28 +9,3 @@ export const sanitizeObject = <T extends object, K extends keyof T>(
 
 	return sanitized;
 };
-
-const mdRenderer = new marked.Renderer();
-mdRenderer.heading = (token) => {
-	const styling = [
-		'text-4xl font-bold text-gray-900 mb-6 border-b pb-2',
-		'text-2xl font-semibold text-gray-800 mt-6',
-	];
-	return `<h${token.depth} class="${styling[token.depth - 1] ?? ''}">${token.text}</h${token.depth}>`;
-};
-mdRenderer.paragraph = (token) => {
-	return `<p class="text-gray-600 mt-2">${token.text}</p>`;
-};
-mdRenderer.list = (token) => {
-	const listItems = token.items
-		.map((item) => `<li class="text-gray-700">${item.text}</li>`)
-		.join('');
-
-	if (token.ordered) {
-		return `<ol class="list-decimal pl-5 space-y-2 mt-4 text-gray-700">${listItems}</ol>`;
-	}
-
-	return `<ul class="space-y-2 mt-4 text-gray-700">${listItems}</ul>`;
-};
-
-export { mdRenderer };
--- src/routes/[lang=lang]/(app)/privacy-policy/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	export let data;
-</script>
-
-<div class="mx-auto mt-10 max-w-3xl rounded-lg bg-white p-6 shadow-lg">
-	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-	{@html data.html}
-</div>
