f2f78e39489e6da109b5c1a9b9eef84661b8fcc9
Sebastiano Racca <sebastiano@racca.me>
2025-02-23 12:31:58 +0100

    feat: add robots.txt

--- src/routes/robots.txt/+server.ts
+++ src/routes/robots.txt/+server.ts
@@ -1,6 +1,22 @@
+import { AVAILABLE_LANGUAGES } from '$project/src/lib/i18n';
+
 export const prerender = true;
 
-// TODO: robots.txt
-export const GET = () => {
-	return new Response();
+export const GET = ({ url }) => {
+	const host = `${url.protocol}//${url.host}`;
+	const sitemaps = AVAILABLE_LANGUAGES.map(lang => `Sitemap: ${host}/${lang}/sitemap.xml`).join("\n")
+
+
+	return new Response(`User-agent: *
+Disallow: /api/
+Disallow: /en/org/
+Disallow: /it/org/
+Disallow: /en/admin/
+Disallow: /it/admin/
+
+Allow: /it/
+Allow: /en/
+
+${sitemaps}
+`);
 };
