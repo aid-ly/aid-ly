b07356559e6a79b16bd2174be0025ac17f658ffe
Sebastiano Racca <sebastiano@racca.me>
2025-02-23 12:31:58 +0100

    feat: add robots.txt

--- src/routes/[lang=lang]/(auth)/+layout.server.ts
+++ src/routes/[lang=lang]/(auth)/+layout.server.ts
@@ -2,6 +2,8 @@ import { redirect } from '@sveltejs/kit';
 
 export const prerender = false;
 
+export const trailingSlash = "always";
+
 export const load = ({ url, locals }) => {
 	if (!locals.user) {
 		redirect(302, `/${locals.lang}/login?to=${url.pathname}`);
