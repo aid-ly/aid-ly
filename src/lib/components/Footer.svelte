82f0c741c8cdfa4b2112acd31c30142e2958dec6
Sebastiano Racca <sebastiano@racca.me>
2025-03-01 22:06:07 +0100

    fix: footer layout

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -7,61 +7,61 @@
 	const year = new Date().getFullYear();
 </script>
 
-<footer class="bg-white p-4 sm:p-6 dark:bg-gray-800">
-	<div class="grid grid-cols-2 gap-8 sm:grid-cols-3 sm:gap-6">
+<footer>
+	<div id="links">
 		<div>
-			<h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+			<h2>
 				{locale.resources.title}
 			</h2>
-			<ul class="text-gray-600 dark:text-gray-400">
-				<li class="mb-4">
+			<ul>
+				<li>
 					<a href={locale.resources.contacts.url}>{locale.resources.contacts.display}</a>
 				</li>
-				<li class="mb-4">
+				<li>
 					<a href={locale.resources.donate.url}>{locale.resources.donate.display}</a>
 				</li>
-				<li class="mb-4">
+				<li>
 					<a href={locale.resources.login.url}>{locale.resources.login.display}</a>
 				</li>
-				<li class="mb-4">
+				<li>
 					<a href={locale.resources.orgPortal.url}>{locale.resources.orgPortal.display}</a>
 				</li>
 			</ul>
 		</div>
 
 		<div>
-			<h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+			<h2>
 				{locale.legal.title}
 			</h2>
-			<ul class="text-gray-600 dark:text-gray-400">
-				<li class="mb-4">
+			<ul>
+				<li>
 					<a href={locale.legal.policy.url}>{locale.legal.policy.display}</a>
 				</li>
-				<li class="mb-4">
+				<li>
 					<a href={locale.legal.terms.url}>{locale.legal.terms.display}</a>
 				</li>
-				<li class="mb-4">
+				<li>
 					<a href={common.license.url}>{locale.legal.license}</a>
 				</li>
 			</ul>
 		</div>
 
 		<div>
-			<h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+			<h2>
 				{locale.credits.title}
 			</h2>
-			<ul class="text-gray-600 dark:text-gray-400">
-				<li class="mb-4">
+			<ul>
+				<li>
 					<a href={common.project.owner.website} title={locale.credits.creator}>
 						{common.project.owner.name}
 					</a>
 				</li>
-				<li class="mb-4">
+				<li>
 					<a href={common.credits.icons} target="_blank" rel="noopener noreferrer"
 						>{locale.credits.icons}</a
 					>
 				</li>
-				<li class="mb-4">
+				<li>
 					<a href={common.credits.maps} target="_blank" rel="noopener noreferrer"
 						>{locale.credits.maps}</a
 					>
@@ -72,8 +72,8 @@
 
 	<hr class="my-6 border-gray-200 sm:mx-auto lg:my-8 dark:border-gray-700" />
 
-	<div class="sm:flex sm:items-center sm:justify-between">
-		<span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+	<div id="socials" class="sm:flex sm:items-center sm:justify-between">
+		<span class="sm:text-center">
 			&copy; 2025 {year === 2025 ? '' : `- ${year}`}
 			<a property="dct:title" rel="cc:attributionURL" href={common.project.url}
 				>{common.project.name} v{version}</a
@@ -99,3 +99,52 @@
 		</div>
 	</div>
 </footer>
+
+<style lang="scss">
+	footer {
+		background-color: var(--color-gray-800);
+		padding: 1rem;
+		width: 100vw;
+
+		> div {
+			&#links {
+				display: flex;
+
+				justify-content: space-between;
+				gap: 20px;
+				flex-direction: row;
+				flex-wrap: wrap;
+
+				> div {
+					padding: 10px;
+
+					h2 {
+						margin-bottom: 1.5rem;
+						font-size: 0.875rem;
+						line-height: 1.25rem;
+						font-weight: 600;
+						color: #ffffff;
+						text-transform: uppercase;
+					}
+
+					ul {
+						color: var(--color-gray-400);
+
+						li {
+							margin-bottom: 1rem;
+						}
+					}
+				}
+			}
+
+			&#socials {
+				span {
+					display: block;
+					font-size: 0.875rem;
+					line-height: 1.25rem;
+					color: var(--color-gray-400);
+				}
+			}
+		}
+	}
+</style>
