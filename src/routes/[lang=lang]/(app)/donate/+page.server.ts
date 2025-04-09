1e5e650687006b6f9783cdfb9964fe36d8864c51
Sebastiano Racca <sebastiano@racca.me>
2025-04-09 16:54:47 +0200

    fix(build): cannot read properties of undefined (reading 'split')

--- src/routes/[lang=lang]/(app)/donate/+page.server.ts
+++ src/routes/[lang=lang]/(app)/donate/+page.server.ts
@@ -1,15 +1,15 @@
 import { env } from '$env/dynamic/private';
 import { AVAILABLE_LANGUAGES, type Language } from '$lib/i18n';
 
-const buttonIds: Record<Language, string> = env.PAYPAL_DONATE_BUTTONS_IDS.split(' ').reduce(
-	(acc, id, indx) => {
-		acc[AVAILABLE_LANGUAGES[indx]] = id;
-		return acc;
-	},
-	{} as Record<Language, string>,
-);
-
 export const load = ({ params }) => {
+	const buttonIds: Record<Language, string> = env.PAYPAL_DONATE_BUTTONS_IDS.split(' ').reduce(
+		(acc, id, indx) => {
+			acc[AVAILABLE_LANGUAGES[indx]] = id;
+			return acc;
+		},
+		{} as Record<Language, string>,
+	);
+
 	return {
 		buttonId: buttonIds[params.lang as Language],
 	};
